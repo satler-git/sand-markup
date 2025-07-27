34a880dfde204fb52427f4ca40d7d787bc5c00ca
satler <satler@satler.dev>
2025-07-28 00:30:23 +0900

    feat(tree-sitter): update query

--- queries/sand/highlights.scm
+++ queries/sand/highlights.scm
@@ -1,28 +1,40 @@
-; === 名前定義 ===
-((name_definition) @keyword.define)
-((identifier_list) @variable)
+; -*- scheme -*-
+; Tree-sitter ハイライト定義 for Sand
+
+; === 識別子 ===
 ((identifier) @variable)
 
-; === セクション ===
-((section) @heading)
-((hashes) @punctuation.special)
-((one_line_str) @string)
+; === 名前定義: `#(`, `)` ===
+((name_definition "#("      ) @punctuation.special)
+((name_definition ")"      ) @punctuation.bracket)
+((identifier_list)          @variable)
 
-; === 全体適用 (apply_all) ===
-((apply_all) @keyword)
+; === セクション: `#alias## title` ===
+((section "#"               ) @punctuation.special)
+((section alias: (identifier) @variable))
+((hashes)                   @punctuation.special)
+; ((one_line_str)             @string)
+
+; === 全体適用 (apply_all): `#{alias,{all,{…}}}` ===
+((apply_all "#"             ) @punctuation.special)
 ((apply_all alias: (identifier) @variable))
-((apply_all content: (string) @string))
+((apply_all "{"             ) @punctuation.bracket)
+((apply_all ","             ) @punctuation.separator)
+((apply_all "}"             ) @punctuation.bracket)
+((apply_all content: (string)     @string))
 
-; === 文定義 (sentence_definition) ===
-((sentence_definition) @constant.builtin)
+; === 文定義 (sentence_definition): `#alias[…][…]` ===
+((sentence_definition "#"         ) @punctuation.special)
 ((sentence_definition alias: (identifier) @variable))
-((sentence_definition content: (string) @string))
+((sentence_definition "["         ) @punctuation.bracket)
+((sentence_definition "]"         ) @punctuation.bracket)
+((sentence_definition content: (string)    @string))
 
-; === セレクター ===
-((selector) @operator)
-((selector (identifier) @variable))
+; === セレクター: `#.` `/` `.` ===
+((selector "#."             ) @punctuation.special)
+((selector "/"              ) @punctuation.special)
+((selector "."              ) @punctuation.special)
+((selector (identifier)     @variable))
 
 ; === 文字列・エスケープ ===
-((string) @string)
-((non_escaped_string) @string)
-
+((string)           @string)
